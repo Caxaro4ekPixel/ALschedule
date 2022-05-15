e5bf2ff0c40ef0d367ce8aafd75416457fb15e74
im4x <korochkin20000@gmail.com>
2022-05-16 04:10:02 +0800

    update

--- api/anilibria.py
+++ api/anilibria.py
@@ -1,28 +1,30 @@
-from urllib import response
+import logging
 import aiohttp
 
 
 class AnilibriaApi:
     def __init__(self, loop):
         self.session = aiohttp.ClientSession(loop=loop)
-        self.loop = loop
+        self._loop = loop
         self._base_url = 'https://api.anilibria.tv/v2/'
 
 
-    async def _request(self, endpoint) -> dict or str:
+    async def _request(self, endpoint):
         if self.session.closed:
-            self.session = aiohttp.ClientSession(loop=self.loop)
+            self.session = aiohttp.ClientSession(loop=self._loop)
 
         async with self.session.get(self._base_url + endpoint) as response:
-            if response.status == 200:
-                return await response.json()
-            else:
-                raise Exception(f'Except status 200, got {response.status}')
-
+            match response.status:
+                case 200:
+                    return await response.json()
+                case 404:
+                    return None
+                case _:
+                    logging.warning("Something went wrong! Status code is {response.status}")
+                    return None
 
     async def get_schedule(self):
         return await self._request('getSchedule')
 
-
-    async def get_title(self, title_id):
+    async def get_title_by_id(self, title_id):
         return await self._request(f'getTitle?id={title_id}')
--- handlers/users/start.py
+++ handlers/users/start.py
@@ -1,26 +1,28 @@
-import logging
 from aiogram import types
-from aiogram.dispatcher.filters import CommandStart, Text
-from aiogram.utils.markdown import hitalic, hcode
-import aiohttp
+from aiogram.dispatcher.filters import CommandStart
+from aiogram.utils.markdown import hcode, hbold
 from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+
 from loader import dp, al_api
 
 
 @dp.message_handler(CommandStart())
 async def bot_start(message: types.Message):
     title_id = message.get_args()
-    title = await al_api.get_title(title_id)
+    title = await al_api.get_title_by_id(title_id)
 
     start_keyboard = InlineKeyboardMarkup()
     start_keyboard.row(
-        InlineKeyboardButton(text="Да", callback_data='1'), 
-        InlineKeyboardButton(text="Нет", callback_data='0')
+        InlineKeyboardButton(text="✅Да", callback_data=f'add-{title_id}'), 
+        InlineKeyboardButton(text="❌Нет", callback_data='cancel')
     )
 
-    if "error" in title:
+    if title:
+        await message.answer(
+            f'{hbold("Релиз: ") + hcode(title.get("names").get("ru"))} ?',   
+            reply_markup=start_keyboard
+        )
+    else:
         await message.answer('🧐Такого релиза не существует!🧐')
         await message.answer('Этот бот используеться в каналах! Для использования нужно прописать /start [id релиза]')
-    else:
-        await message.answer(f'Релиз: {title.get("names").get("ru")}?\nID: {title_id}', reply_markup=start_keyboard)
 
