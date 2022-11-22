bd7e4b3a2ac7e30a2a4dc338a7a42d69537b66d0
im4x <korochkin20000@gmail.com>
2022-11-22 21:34:59 +1300

    fix регистрации, добавлено описание

--- asuna_bot/handlers/registration.py
+++ asuna_bot/handlers/registration.py
@@ -1,23 +1,31 @@
+"""
+Ботом могут пользоваться только зарегистрированные пользователи.
+Регистрация происходит путем пересылки админом любого сообщения пользователя,
+если настройки приватности не позволяют добавить пользователя, 
+просим на время изменить настройки приватности.
+"""
+
 from aiogram import Router
 from aiogram.types import Message
 from asuna_bot.filters.admins import AdminFilter
 
 from asuna_bot.filters.chat_type import ChatTypeFilter
-from asuna_bot.db.mongo import add_user
+from asuna_bot.db.mongo import mongo
 
 admin_router = Router()
 admin_router.message.filter(AdminFilter(), ChatTypeFilter("private"))
 
-# Админы дают доступ
+
 @admin_router.message()
 async def accept_user(msg: Message):
-    if not msg.forward_from:
-        name = msg.forward_sender_name
-        await msg.answer(f"Не могу добавить {name} из-за настроек приватности 😔")
+    if msg.forward_from:
+        username = msg.forward_from.username
+        user_id = msg.forward_from.id
+        full_name = msg.forward_from.full_name        
+        mongo.add_user(user_id, full_name, username)
+        await msg.answer(f"{full_name} / @{username}\nДобавлен в базу! 👍")
         return
     
-    username = msg.forward_from.username
-    user_id = msg.forward_from.id
-    full_name = msg.forward_from.full_name        
-    add_user(user_id, full_name, username)
-    await msg.answer(f"{full_name} / @{username}\nДобавлен в базу! 👍")
+    if msg.forward_sender_name:
+        name = msg.forward_sender_name
+        await msg.answer(f"Не могу добавить {name} из-за настроек приватности 😔")
